bd370e0dac811832c9bfd4b7d38eb4535de54146
Shakoor Hussain Attari <binmushtaq@gmail.com>
2025-09-20 12:03:09 +0400

    fix: Update router link for Projects and import RouterModule in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 import { MetaService } from './core/services/meta.service';
 import { PerformanceService } from './core/services/performance.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
